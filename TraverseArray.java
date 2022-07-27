e8a0a04feedf80e21099a747a1d4040832d4a2f8
unknown <renukakom31@gmail.com>
2022-07-27 09:08:08 +0530

    Traverse Array

---
+++ TraverseArray.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.practice;
+//Java program to traverse an array
+public class TraverseArray {
+    public static void main(String[] args) {
+        int array[] = {2,4,6,8,10};
+        for (int traverse : array) {
+            System.out.println(traverse);
+        }
+    }
+}
